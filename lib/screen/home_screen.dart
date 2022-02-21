85f189a48ca905bc4a9891cf3b5dc3a789360b1c
Ji Ho Choi <jihochoi@Jiui-MacBookPro.local>
2022-02-21 13:24:37 +0900

    done

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final Color color;
+
+  const HomeScreen({
+    required this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50.0,
+      height: 50.0,
+      color: color,
+    );
+  }
+}
